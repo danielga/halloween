e0781f992e2ec4334291106cbdd60977fcf41431
Daniel <danielga@outlook.pt>
2017-10-29 21:01:39 +0000

    Added weapon entity for HHH.
    Now we can have a specific weapon name shown in the deaths ticker.

---
+++ lua/entities/lua_weapon_hhhh.lua
@@ -0,0 +1,23 @@
+ENT.Base = "base_anim"
+ENT.Author = "MetaMan"
+ENT.PrintName = "Horseless Headless Horsemann's Headtaker"
+
+ENT.Spawnable = false
+ENT.AdminSpawnable = false
+
+ENT.AutomaticFrameAdvance = true
+
+ENT.PhysgunDisabled = true
+ENT.m_tblToolsAllowed = {}
+
+function ENT:CanConstruct()
+	return false
+end
+
+function ENT:CanTool()
+	return false
+end
+
+function ENT:Initialize()
+	self:SetModel("models/weapons/c_models/c_bigaxe/c_bigaxe.mdl")
+end
